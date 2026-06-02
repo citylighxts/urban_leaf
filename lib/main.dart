0f0440334c7fb092c448922002b33da92ead3a2e
hyhnuka <halizaapalwan@gmail.com>
2026-06-02 07:01:16 +0700

    implementasi kamera dan pick galery, model masih ongoing

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,24 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'core/theme/app_theme.dart';
-import 'pages/auth/login_page.dart'; // 1. Pastikan import ke LoginPage buatanmu
+import 'pages/auth/login_page.dart'; 
+import 'package:camera/camera.dart'; // Import paket kamera
 
-void main() async { // 2. Tambahkan kata kunci 'async' di sini
+// PENTING: Deklarasikan variabel global cameras di atas fungsi main agar bisa di-share ke AI Scanner page
+List<CameraDescription> cameras = [];
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // 3. Inisialisasi Firebase sebelum runApp dijalankan
+  // 1. Ambil daftar hardware lensa kamera fisik yang tersedia di HP Samsung kamu
+  try {
+    cameras = await availableCameras();
+    print("📸 Kamera Fisik Berhasil Didaftarkan: ${cameras.length} lensa ditemukan.");
+  } catch (e) {
+    print("⚠️ Gagal mendeteksi sensor kamera fisik: $e");
+  }
+  
+  // 2. Inisialisasi Firebase sebelum runApp dijalankan
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -29,8 +41,8 @@ class UrbanLeafApp extends StatelessWidget {
     return MaterialApp(
       title: 'UrbanLeaf AI',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme, // Tetap menggunakan tema lightTheme bawaanmu
-      home: const LoginPage(), // 4. Ubah dari MainNavigation() ke LoginPage()
+      theme: AppTheme.lightTheme, 
+      home: const LoginPage(), 
     );
   }
 }
